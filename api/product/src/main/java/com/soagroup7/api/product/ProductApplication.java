e360f8145c2bab5b009587490c299dd1afed05e7
Unknown <59070103@it.kmitl.ac.th>
2019-03-12 01:57:46 +0700

    Add Product service API for sample

---
+++ api/product/src/main/java/com/soagroup7/api/product/ProductApplication.java
@@ -0,0 +1,13 @@
+package com.soagroup7.api.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class, args);
+    }
+
+}
