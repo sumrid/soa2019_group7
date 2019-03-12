6d3c956b750b19cf4c666208547c3e614ca14d6a
Oasong <59070098@it.kmitl.ac.th>
2019-03-12 17:18:24 +0700

    Add Stock Service

---
+++ api/stock/src/main/java/com/oasong/stock/model/Stock.java
@@ -0,0 +1,74 @@
+package com.oasong.stock.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Stock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String date;
+
+    private String name;
+    private int quantity;
+    private double price;
+    private String status;
+
+    public Stock(int id, String name, String date, int quantity, double price, String status) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.quantity = quantity;
+        this.price = price;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
