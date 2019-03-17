ac2db3185a089443b9cf50383d506a631165953e
Sedah <phensiritn1@gmail.com>
2019-03-17 21:12:20 +0700

    add user repository and service test

--- src/main/java/com/example/pos/api/user/repository/UserRepository.java
+++ src/main/java/com/example/pos/api/user/repository/UserRepository.java
@@ -4,7 +4,9 @@ import com.example.pos.api.user.model.Employee;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<Employee, Long> {
-
+    Optional<Employee> findByname(String name);
 }
