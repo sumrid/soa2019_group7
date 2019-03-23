4a58060bc7f4ad5cf00b8d8442c63bc17386f130
Unknown <59070103@it.kmitl.ac.th>
2019-03-24 03:37:26 +0700

    Add Product Service
    function test driven status in process.

---
+++ src/main/java/com/example/pos/api/product/controller/ProductController.java
@@ -0,0 +1,62 @@
+package com.example.pos.api.product.controller;
+
+
+import com.example.pos.api.product.model.Product;
+import com.example.pos.api.product.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    @Autowired
+    ProductService productService;
+
+
+    @GetMapping()
+    public List<Product> getProduct() {
+        return productService.retrieveProduct();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getCustomer(@PathVariable Long id) {
+        Optional<Product> product = productService.retrieveProduct(id);
+        if(!product.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(product);
+    }
+
+
+    @PostMapping("/save")
+    public ResponseEntity<?> postCustomer(@Valid @RequestBody Product body) {
+        Product product = productService.saveProduct(body);
+        return ResponseEntity.status(HttpStatus.CREATED).body(product);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> putProduct(@PathVariable Long id, @Valid @RequestBody Product body) {
+        Optional<Product> product = productService.updateProduct(id, body);
+        if(!product.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteProduct(@PathVariable Long id) {
+        if(!productService.deleteProduct(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().build();
+    }
+
+
+}
---
+++ src/main/java/com/example/pos/api/product/service/ProductService.java
@@ -0,0 +1,58 @@
+package com.example.pos.api.product.service;
+
+import com.example.pos.api.product.model.Product;
+import com.example.pos.api.product.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    private ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+
+    public List<Product> retrieveProduct(){
+        return (List<Product>) productRepository.findAll();
+    }
+
+    public Optional<Product> retrieveProduct(Long id){
+        return productRepository.findById(id);
+    }
+
+    public Optional<Product> retrieveProduct(String name){
+        return productRepository.findByName(name);
+    }
+
+    public Product saveProduct(Product product){
+        product.setId(null);
+        return productRepository.save(product);
+    }
+
+    public Optional<Product> updateProduct(Long id, Product product){
+        Optional<Product> productOptional = productRepository.findById(id);
+        if(!productOptional.isPresent()){
+            return productOptional;
+        }
+        product.setId(id);
+        return Optional.of(productRepository.save(product));
+    }
+
+    public boolean deleteProduct(Long id){
+        try {
+            productRepository.deleteById(id);
+            return true;
+
+        } catch (EmptyResultDataAccessException e){
+            return false;
+        }
+    }
+
+}
