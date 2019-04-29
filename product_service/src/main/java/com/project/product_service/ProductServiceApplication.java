d4c481329c9d5a62f3b923ba6edd68e610de2eff
Sumrid Klinkanha <59070174@it.kmitl.ac.th>
2019-04-30 01:16:01 +0700

    connect product-service to eureka-service

--- product_service/src/main/java/com/project/product_service/ProductServiceApplication.java
+++ product_service/src/main/java/com/project/product_service/ProductServiceApplication.java
@@ -2,8 +2,16 @@ package com.project.product_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+@EnableHystrix
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
