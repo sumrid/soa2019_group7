768f1e5c3377fbc0cdc773a823f02811fb75c756
Sumrid Klinkanha <59070174@it.kmitl.ac.th>
2019-05-04 20:14:56 +0700

    update stock service
    - add Dockerfile
    - connect to eureka service

--- stock_service/src/main/java/com/project/stock_service/StockServiceApplication.java
+++ stock_service/src/main/java/com/project/stock_service/StockServiceApplication.java
@@ -2,7 +2,15 @@ package com.project.stock_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
+@EnableHystrix
+@EnableEurekaClient
+@EnableCircuitBreaker
+@EnableDiscoveryClient
 @SpringBootApplication
 public class StockServiceApplication {
 
@@ -11,3 +19,5 @@ public class StockServiceApplication {
 	}
 
 }
+
+
