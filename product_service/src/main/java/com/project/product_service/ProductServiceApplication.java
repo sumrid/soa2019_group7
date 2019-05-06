3053fc7f33526bc48fe2e42413acce415272dafd
Sumrid Klinkanha <59070174@it.kmitl.ac.th>
2019-05-06 23:16:10 +0700

    Update ProductServiceApplication.java
    fix build error

--- product_service/src/main/java/com/project/product_service/ProductServiceApplication.java
+++ product_service/src/main/java/com/project/product_service/ProductServiceApplication.java
@@ -6,9 +6,6 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -19,21 +16,5 @@ public class ProductServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-
-			private final long MAX_AGE_SECS = 3600;
-
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("*")
-						.allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
-						.maxAge(MAX_AGE_SECS);
-			}
-		};
 	}
 }
