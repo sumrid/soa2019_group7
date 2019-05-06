72c8edb5a2b1fd7ba77f0af5f443d8e49b316f14
Sumrid Klinkanha <59070174@it.kmitl.ac.th>
2019-05-06 09:41:46 +0700

    ต่อ user service กับ eureka

--- user_service/src/main/java/com/example/userservice/UserserviceApplication.java
+++ user_service/src/main/java/com/example/userservice/UserserviceApplication.java
@@ -3,12 +3,16 @@ package com.example.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 @EntityScan(basePackageClasses = {
         UserserviceApplication.class,
         Jsr310JpaConverters.class
